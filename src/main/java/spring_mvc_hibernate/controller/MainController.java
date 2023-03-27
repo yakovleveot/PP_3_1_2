1516563d65df2cf2019ac5a2cdfa95d95f4aa34e
yakovleveot <yakovlev.eot@ya.ru>
2023-03-27 15:43:08 +0300

    Использую PostMapping. При обновлении юзера использую отдельный метод updateUserData (ранее был использован метод saveUser как для добавления нового, как и для обновления)

--- src/main/java/spring_mvc_hibernate/controller/MainController.java
+++ src/main/java/spring_mvc_hibernate/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import spring_mvc_hibernate.model.User;
 import spring_mvc_hibernate.service.UserService;
@@ -23,29 +24,31 @@ public class MainController {
         return "table";
     }
 
-    @GetMapping("/addUser")
+    @PostMapping("/addUser")
     public String addUser(Model model) {
         model.addAttribute("userData", new User());
         return "add_user";
     }
 
-    @GetMapping("/saveUser")
+    @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute("userData") User user) {
-        if (user.getId() == 0) {
             userService.addUser(user);
-        } else {
-            userService.updateUser(user);
-        }
         return "redirect:/";
     }
 
-    @GetMapping("/updateUser")
+    @PostMapping("/updateUser")
     public String updateUser(@RequestParam("id") int id, Model model) {
         model.addAttribute("userData", userService.getUser(id));
-        return "add_user";
+        return "update_user";
     }
 
-    @GetMapping("/deleteUser")
+    @PostMapping("/updateUserData")
+    public String updateUserData(@ModelAttribute("userData") User user) {
+            userService.updateUser(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/deleteUser")
     public String deleteUser(@RequestParam("id") int id) {
         userService.deleteUser(id);
         return "redirect:/";
